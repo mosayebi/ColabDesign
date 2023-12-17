52a5aef06251d03c82e9224239d2ba2bded7582c
Majid Mosayebi <majid.mosayebi@nanoporetech.com>
2023-12-17 23:50:29 +0000

    clean-up

--- colabdesign/rf/utils.py
+++ colabdesign/rf/utils.py
@@ -329,20 +329,17 @@ def get_bias_and_decoding_order(weights: np.ndarray):
 
 def _convert_mutation_probs(mutation, target_alphabet):
     """convert probs from mutation object to correctly ordered priors, filling out positions other than mutations sites.
-    returns esm_priors with shape [len(seq), len(alphabet)]
+    returns esm_priors with shape [len(seq), len(target_alphabet)]
     """
-    ignore_tokens = [x for x in mutation.alphabet_tokens if x not in target_alphabet]
-    assert len(mutation.alphabet_tokens) - len(ignore_tokens) == len(target_alphabet)
     idxs_map = np.array([mutation.alphabet.get_idx(a) for a in target_alphabet])
-    seq = mutation.data[0][0][1] if mutation.is_msa else mutation.data[0][1]
-
+    WT_seq = mutation.data[0][0][1] if mutation.is_msa else mutation.data[0][1]
     # fill positions other than mutation sites and the WT residues at fixed positions with zero
-    esm_priors = np.zeros((len(seq), len(target_alphabet)))
+    esm_priors = np.zeros((len(WT_seq), len(target_alphabet)))
     for s, p in zip(mutation.mutation_sites, mutation.residue_probs[0][:, idxs_map]):
         esm_priors[s, :] = p
-    for idx, res in enumerate(seq):
-        if idx not in mutation.mutation_sites:
-            esm_priors[idx, target_alphabet.index(res)] = 1
+    for i, res in enumerate(WT_seq):
+        if i not in mutation.mutation_sites:
+            esm_priors[i, target_alphabet.index(res)] = 1
     return esm_priors
 
 
